08781058fb34119d7302471983e515ac37870d3a
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2026-02-16 22:24:40 +0900

    2−50までの愚直バージョン

--- prime/src/main.rs
+++ prime/src/main.rs
@@ -1,39 +1,47 @@
 fn main() {
-    let prime = PrimeSieveV1::new();
+    let mut sieve = PrimeSieveV1::new();
 
-    println!("Hello, world! {}", prime.sieve[0]);
+    sieve.run();
+
+    for prime in sieve.primes {
+        println!("{}", prime);
+    }
 }
 
 struct PrimeSieveV1 {
     sieve: Vec<bool>,
-    #[allow(dead_code)]
-    prime: Vec<u32>,
+    primes: Vec<usize>,
 }
 
 impl PrimeSieveV1 {
     pub fn new() -> Self {
         PrimeSieveV1 {
-            sieve: vec![false; 256],
-            prime: vec![2u32],
+            sieve: vec![false; 50],
+            primes: vec![],
         }
     }
 
-    #[allow(dead_code)]
-    pub fn make_sieve(&mut self, step: usize) {
+    fn mark_multiples(&mut self, step: usize) {
         let len = self.sieve.len();
 
-        (step..len)
-            .step_by(step)
-            .for_each(|x| self.sieve[x] = true);
+        (step..len).step_by(step).for_each(|x| self.sieve[x] = true);
     }
 
-    #[allow(dead_code)]
-    pub fn find_next_prime(&self, index: usize) -> Option<usize> {
-        return self
-            .sieve
+    fn next_unmarked(&self, index: usize) -> Option<usize> {
+        self.sieve
             .iter()
             .skip(index)
             .position(|&x| x == false)
-            .map(|x| x + index);
+            .map(|x| x + index)
+    }
+
+    fn run(&mut self) {
+        let mut current = Some(2usize);
+
+        while let Some(x) = current {
+            self.primes.push(x);
+            self.mark_multiples(x);
+            current = self.next_unmarked(x);
+        }
     }
 }
