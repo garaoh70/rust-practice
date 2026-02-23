e62b96aae203209bc2f901c420b3bed2a217aa01
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2026-02-23 23:04:12 +0900

    V2のテストも追加

--- prime/src/domain/prime.rs
+++ prime/src/domain/prime.rs
@@ -50,3 +50,22 @@ impl PrimeV2 {
         self.elapsed
     }
 }
+
+#[cfg(test)]
+mod test_v2 {
+    use super::*;
+
+    #[test]
+    fn test_prime_numbers_less_than_50() {
+        // Arrange
+        let mut prime = PrimeV2::new();
+        let expected = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+
+        // Arrange
+        prime.run(50);
+        let actual = prime.primes;
+
+        // Assert
+        assert_eq!(actual, expected, "50未満の素数は一致しません");
+    }
+}
