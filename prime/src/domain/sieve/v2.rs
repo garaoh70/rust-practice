2974f669a886ec39120b7574eb2c605046a68f9b
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2026-03-08 10:47:39 +0900

    非同期処理

--- prime/src/domain/sieve/v2.rs
+++ prime/src/domain/sieve/v2.rs
@@ -54,8 +54,8 @@ mod test_v2 {
         let expected = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
 
         // Arrange
-        prime.run();
-        let actual = prime.values();
+        let result = prime.run();
+        let actual = result.primes;
 
         // Assert
         assert_eq!(actual, expected, "50未満の素数は一致しません");
