51f5ef69ae1c48366210feef392c85af5f8623de
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2026-03-08 10:38:36 +0900

    CLI強化

--- prime/src/cli.rs
+++ prime/src/cli.rs
@@ -20,15 +20,40 @@ pub struct Arguments {
     )]
     pub sieve: usize,
 
-    /// Maximum number (clamped to 2..=20_000_000_000)
+    /// Maximum number
     #[arg(
         short,
         long,
         default_value_t = DEFAULT_COUNT,
-        value_parser = ValueParser::new(|s: &str| -> Result<usize, String> {
-            let v: usize = s.parse().unwrap_or(DEFAULT_COUNT);
-            Ok(v.clamp(MIN_COUNT, MAX_COUNT))
-        })
+        value_parser = parse_maximum_number
     )]
-    pub count: usize,
+    pub number: usize,
+
+    /// Show result
+    #[arg(long, default_value_t = false)]
+    pub show: bool,
+}
+
+fn parse_maximum_number(input: &str) -> Result<usize, String> {
+    let numeric_pos = input
+        .find(|c: char| !c.is_numeric() && c != '.')
+        .unwrap_or(input.len());
+    let (num_part, unit_part) = input.split_at(numeric_pos);
+
+    let value: f64 = num_part.parse().expect("parse error");
+    let multiplier = match unit_part.to_ascii_uppercase().as_str() {
+        "K" => 1_000_f64,
+        "M" => 1_000_000_f64,
+        "G" => 1_000_000_000_f64,
+        "T" => 1_000_000_000_000_f64,
+        "" => 1_f64,
+        _ => return Err("unknown unit".to_string()),
+    };
+
+    let maximum_number = (value * multiplier) as usize;
+    if (MIN_COUNT..=MAX_COUNT).contains(&maximum_number) {
+        Ok(maximum_number)
+    } else {
+        Err("out of range".to_string())
+    }
 }
--- prime/src/main.rs
+++ prime/src/main.rs
@@ -8,8 +8,8 @@ use cli::Arguments;
 
 fn main() {
     let args = Arguments::parse();
+    let limit = args.number;
 
-    let limit = args.count;
     let sieve: Box<dyn SieveGenerator> = match args.sieve {
         1 => Box::new(SieveV1::new(limit)),
         2 => Box::new(SieveV2::new(limit)),
@@ -32,15 +32,19 @@ fn main() {
         limit
     };
 
-    println!("🦀 Rust Prime Sieve Report");
-    println!("--------------------------------");
-    println!("impl : {}", impl_name);
-    println!("limit: {}", limit);
-    println!("");
-    println!("π(n)       = {}", prime_length);
-    println!("min_prime  = {}", min_prime);
-    println!("max_prime  = {}", max_prime);
-    println!("elapsed    = {:.3}s", (elapsed_msec as f64) / 1000f64);
-    println!("throughput = {} n/s", throughput);
-    println!("--------------------------------");
+    if args.show {
+        println!("🦀 Rust Prime Sieve Report");
+        println!("--------------------------------");
+        println!("impl : {}", impl_name);
+        println!("limit: {}", limit);
+        println!("");
+        println!("π(n)       = {}", prime_length);
+        println!("min_prime  = {}", min_prime);
+        println!("max_prime  = {}", max_prime);
+        println!("elapsed    = {:.3}s", (elapsed_msec as f64) / 1000f64);
+        println!("throughput = {} n/s", throughput);
+        println!("--------------------------------");
+    } else {
+        println!("elapsed    = {:.3}s", (elapsed_msec as f64) / 1000f64);
+    }
 }
